a9b4113ef5f3e6ee8dd1f6384cfd9aa02ee5bb7d
张宝冬 <baodong.zhang@argrace.ai>
2020-11-11 18:18:05 +0800

    add mobileHome

--- src/views/mobile/mobileHome/index.vue
+++ src/views/mobile/mobileHome/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="mobileHome">
 
-    <router-view />
+    <router-view class="routerView" />
     <cube-tab-bar
-      v-model="selectedLabelDefault"
+      class="tabBar"
+      v-model="selectedLabel"
       :data="tabs"
       @click="clickHandler"
       @change="changeHandler"
@@ -13,33 +14,51 @@
 </template>
 <script lang="ts">
 import { Component, Provide, Vue } from 'vue-property-decorator';
+@Component
 export default class MobileHome extends Vue {
+  @Provide() private selectedLabel: string = 'home';
   @Provide() private test: string = 'testss';
-  @Provide() private selectedLabelDefault: string = 'wiki';
   @Provide() private tabs: object[] = [
     {
-      label: 'Home',
+      label: '',
       icon: 'cubeic-home',
+      value: 'home',
     },
     {
-      label: 'Like',
-      icon: 'cubeic-like',
-    },
-    {
-      label: 'Vip',
+      label: '',
       icon: 'cubeic-vip',
+      value: 'like',
     },
     {
-      label: 'Me',
+      label: '',
+      icon: 'cubeic-picture',
+      value: 'vip',
+    },
+    {
+      label: '',
       icon: 'cubeic-person',
+      value: 'me',
     },
   ];
-  private clickHandler(label: string): void {
-    console.log(label);
+  private clickHandler(label: string) {}
+  private changeHandler(item: any) {
+    console.log(item, this.test, 222);
   }
-  private changeHandler(): void {}
 }
 </script>
 <style lang="scss" scoped>
 @import './index';
+</style>
+<style lang="scss" >
+.mobileHome {
+  // .cube-tab {
+  //   height: 10vh;
+  // }
+  // .cubeic-home {
+  //   font-size: 1.5rem;
+  // }
+  .cube-tab {
+    font-size: 2.2rem;
+  }
+}
 </style>
