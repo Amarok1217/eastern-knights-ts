236865d980bf9d14a07ea2f919ad8fe75cf41502
张宝冬 <baodong.zhang@argrace.ai>
2020-11-12 14:09:03 +0800

    init react

---
+++ eastern-knights-ts/src/index.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import Route from './router/index';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// const render = (Component: any) => {
+//   ReactDOM.render(
+//       <Component />, 
+//       document.getElementById('root') as HTMLElement
+//   )
+// }
+// render(Route)
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ eastern-knights-ts/src/router/index.tsx
@@ -0,0 +1,9 @@
+import App from '@/App.tsx'
+export const routes: object = {
+    path: '/',
+    component: App,
+    childRoutes: [
+        // { path: 'about', component: About },
+        // { path: 'inbox', component: Inbox },
+    ]
+}
