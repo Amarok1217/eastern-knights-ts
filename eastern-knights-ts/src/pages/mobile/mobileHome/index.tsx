06fe2566a43b6e2ce1a6533871f462dbec2e8f7d
张宝冬 <baodong.zhang@argrace.ai>
2020-11-12 18:42:10 +0800

    add less

--- eastern-knights-ts/src/pages/mobile/mobileHome/index.tsx
+++ eastern-knights-ts/src/pages/mobile/mobileHome/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import './index'
+import './index.less'
+import { Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
 
 function MobileHome() {
   return (
     <div className='mobileHome'>
+      <Button>Start</Button>
       <section className='demo'>mobile</section>
     </div>
   )
